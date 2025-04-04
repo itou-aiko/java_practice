062ed651559deda25c08db981cc26d48b1401ebd
itou <aikodon0726@icloud.com>
2025-04-04 16:09:52 +0000

    practice

--- 4/4_07.js
+++
@@ -1,6 +0,0 @@
-let a = -5;
-if (a >= 0) {
-    console.log('a=' + a + 'は、正の整数です'); 
-} else {
-    console.log('a=' + a + 'は、負の整数です'); 
-}
---
+++ 4/4_11.js
@@ -0,0 +1,56 @@
+/*switch (式) {
+    case ラベル1:
+        式とラベル１が等しいときに行われる処理
+           break;
+    case ラベル2:
+        式とラベル２が等しいときに行われる処理
+        break;
+    case ラベル3:
+        式とラベル3が等しいときに行われる処理
+     break;
+    ・・・
+    default:
+        式がどのラベルとも一致     しなかった場合
+}*/
+
+let mon = 2;
+switch (mon) {
+    case 1:
+        console.log('January'); 
+        break;
+    case 2:
+        console.log('February'); 
+        break;
+    case 3:
+        console.log('March'); 
+        break;
+    case 4:
+        console.log('April'); 
+        break;
+    case 5:
+        console.log('May'); 
+        break;
+    case 6:
+        console.log('June'); 
+        break;
+    case 7:
+        console.log('July'); 
+        break;
+    case 8:
+        console.log('August'); 
+        break;
+    case 9:
+        console.log('September'); 
+        break;
+    case 10:
+        console.log('October'); 
+        break;
+    case 11:
+        console.log('November'); 
+        break;
+    case 12:
+        console.log('December'); 
+        break;
+    default:
+        console.log('不明です'); 
+}
